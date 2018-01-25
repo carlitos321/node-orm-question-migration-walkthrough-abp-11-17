4978bbf69602eb45b2723463192dc7fa41768155
carlitos321 <ca.cabrera321@gmail.com>
2018-01-25 22:10:00 +0000

    automatically backed up by learn

--- models/Question.js
+++ models/Question.js
@@ -4,9 +4,10 @@ const db = require("../config/db")
 
 class Question{
   static CreateTable() {
-    
+    return new Promise(function(resolve){
+      resolve("This Does Nothing!")
+    })
   }
-
 }
 
 
