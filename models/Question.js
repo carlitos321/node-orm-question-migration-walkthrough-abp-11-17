351d23b7515bd9aa1d9500a70dd875c83e629344
carlitos321 <ca.cabrera321@gmail.com>
2018-01-25 22:14:47 +0000

    automatically backed up by learn

--- models/Question.js
+++ models/Question.js
@@ -6,7 +6,7 @@ class Question{
   static CreateTable() {
     return new Promise(function(resolve){
       const sql = `CREATE TABLE questions
-      (id INTEGER PRIMARY KEY)`
+      (id INTEGER PRIMARY KEY, content TEXT)`
       db.run(sql, function(){
         resolve("questions table created")
       })
